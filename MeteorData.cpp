106d8b02e9c987b5d8f16125d30e70aacc95a662
MasterBaiter6969 <patrik.blaskovic0@gmail.com>
2020-04-07 17:04:09 +0200

    Promjenjeni fileovi s viškom teksta

--- MeteorData.cpp
+++ MeteorData.cpp
@@ -2,7 +2,6 @@
 #include "MeteorData.h"
 using namespace std;
 
-<<<<<<< HEAD
 Meteor_Data::Meteor_Data(string _database, int _meteor_number, string _date, string _time, string _beta_heliocentric, string _solar_longitude, string _excentricity, string _inclination, string _peri, string _node) {
     database = _database;
     meteor_number = _meteor_number;
@@ -15,17 +14,3 @@ Meteor_Data::Meteor_Data(string _database, int _meteor_number, string _date, str
     peri = stod(_peri);
     node = stod(_node);
 }
-=======
-Meteor_Data :: Meteor_Data(string _database, int _meteor_number, string _date, string _time, string _beta_heliocentric, string _solar_longitude, string _excentricity, string _inclination, string _peri, string _node){
-  database = _database;
-  meteor_number = _meteor_number;
-  date = _date;
-  time = _time;
-  beta_heliocentric = stod(_beta_heliocentric);
-  solar_longitude = stod(_solar_longitude);
-  excentricity = stod(_excentricity);
-  inclination = stod(_inclination);
-  peri = stod(_peri);
-  node = stod(_node);
-}
->>>>>>> 152432ed337cd0ed0b750e2ff651a0d2a08b8081
--- MeteorData.h
+++ MeteorData.h
@@ -2,13 +2,8 @@
 using namespace std;
 
 class Meteor_Data {
-<<<<<<< HEAD
 public:
     string database;
-=======
-  public:
-    string database; 
->>>>>>> 152432ed337cd0ed0b750e2ff651a0d2a08b8081
     int meteor_number;
     string date;
     string time;
@@ -19,10 +14,5 @@ public:
     double peri;
     double node;
 
-<<<<<<< HEAD
     Meteor_Data(string _database, int meteor_number, string date, string time, string beta_heliocentric, string solar_longitude, string excentricity, string inclination, string peri, string node);
 };
-=======
-    Meteor_Data(string database, int meteor_number, string date, string time, string beta_heliocentric, string solar_longitude, string excentricity, string inclination, string peri, string node);
-    };
->>>>>>> 152432ed337cd0ed0b750e2ff651a0d2a08b8081
